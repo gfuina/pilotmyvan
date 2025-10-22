8820052c85d25caa0148a626e799da610869950e
Gianni Fuina <fuina.gianni@gmail.com>
2025-10-22 10:02:33 +0200

    Remove service worker configuration from next.config.ts to streamline PWA setup and enhance runtime caching settings.

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,6 @@ export default withPWA({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
-  sw: "sw.js",
   runtimeCaching: [
     {
       urlPattern: /^https?.*/,
